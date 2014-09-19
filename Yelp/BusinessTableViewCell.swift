a0eb3377f1b98e7f556844459111c1f4517e85fe
Ashish <ashish@a.patel7734@gmail.com>
2014-09-19 08:52:25 -0700

    creating and wiring search screen UI with controller

---
+++ Yelp/BusinessTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  BusinessTableViewCell.swift
+//  Yelp
+//
+//  Created by Ashish Patel on 9/18/14.
+//  Copyright (c) 2014 Average Techie. All rights reserved.
+//
+
+import UIKit
+
+class BusinessTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    @IBOutlet weak var thumImageView: UIImageView!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yelp/ViewController.swift
+++ Yelp/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource  {
+    
+    @IBOutlet var businessTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //MARK - delegates
+        businessTableView.delegate = self
+        businessTableView.dataSource = self
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func filterButtonClicked(sender: AnyObject) {
+        println("Filter clicked")
+    }
+    
+    
+    //MARK - tableview methods
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("businesscell") as BusinessTableViewCell
+        cell.locationNameLabel.text = "1. Basil Thai Restaurant and Bar"
+        cell.addressLabel.text = "1175 Folsom st, Soma"
+        cell.categoryLabel.text = "Thai, Seafood, Salad"
+        cell.reviewsLabel.text = "469 Reviews"
+        cell.distanceLabel.text = "0.07mi"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
 }
 
